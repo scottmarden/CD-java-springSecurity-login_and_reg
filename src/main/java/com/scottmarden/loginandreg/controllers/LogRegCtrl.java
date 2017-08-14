db617a97e4ebf972c830361e88e7c2278ef1c771
Scott <marden.scott@gmail.com>
2017-08-14 13:56:22 -0700

    Added authorization functionality that allows for admin users who can delete other users and promote users to admin status.

--- src/main/java/com/scottmarden/loginandreg/controllers/LogRegCtrl.java
+++ src/main/java/com/scottmarden/loginandreg/controllers/LogRegCtrl.java
@@ -1,6 +1,7 @@
 package com.scottmarden.loginandreg.controllers;
 
 import java.security.Principal;
+import java.util.List;
 
 import javax.validation.Valid;
 
@@ -8,10 +9,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.scottmarden.loginandreg.models.Role;
 import com.scottmarden.loginandreg.models.User;
 import com.scottmarden.loginandreg.services.UserService;
 import com.scottmarden.loginandreg.validator.UserValidator;
@@ -49,8 +52,15 @@ public class LogRegCtrl {
 		if(result.hasErrors()) {
 			return "landingPage.jsp";
 		}
-		userService.saveWithUserRole(user);
-		return "redirect:/home";
+		List<Role> adminRole = userService.findRoleByName("ROLE_ADMIN");
+		if(adminRole.get(0).getUsers().size() == 0 ) {
+			userService.saveWithAdminRole(user);
+			return "redirect:/home";
+		}else {
+			userService.saveWithUserRole(user);
+			return "redirect:/home";
+		}
+
 	}
 	
 	@RequestMapping(value="/login")
@@ -68,8 +78,47 @@ public class LogRegCtrl {
 	public String home(Principal principal, Model model) {
 		String username = principal.getName();
 		User currentUser = userService.findByUsername(username);
+		if(currentUser.getRoles().get(0).getName().equals("ROLE_ADMIN")) {
+			model.addAttribute("adminFlag", "admin");
+		}
 		model.addAttribute("currentUser", currentUser);
 		return "homePage.jsp";
+	}
+	
+	@RequestMapping(value= "/testing")
+	public String testCode(Principal principal) {
+		List<Role> roles = userService.findAllRoles();
+		System.out.println("Roles: " + roles);
+		List<Role> adminRole = userService.findRoleByName("ROLE_ADMIN");
+		System.out.println("Number of adminRole users: " + adminRole.get(0).getUsers().size());
+		
+		
+		
+		
+		return "test.jsp";
+	}
+	
+	@RequestMapping(value="/admin")
+	public String adminPage(Principal principal, Model model) {
+		String username = principal.getName();
+		model.addAttribute("currentUser", userService.findByUsername(username));
+		List<User> users = userService.findRoleByName("ROLE_USER").get(0).getUsers();
+		List<User> admins = userService.findRoleByName("ROLE_ADMIN").get(0).getUsers();
+		model.addAttribute("users", users);
+		model.addAttribute("admins", admins);
+		return "adminPage.jsp";
+	}
+	
+	@RequestMapping(value="/admin/delete/{user_id}")
+	public String destroyUser(@PathVariable("user_id") Long user_id) {
+		userService.destroyUser(user_id);
+		return "redirect:/admin";
+	}
+	
+	@RequestMapping(value="/admin/create_admin/{user_id}")
+	public String createAdmin(@PathVariable("user_id") Long user_id) {
+		userService.createAdmin(user_id);
+		return "redirect:/admin";
 	}
 
 }
